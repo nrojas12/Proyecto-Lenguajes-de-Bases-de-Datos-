3caeca87ddcf420baba107ce1043d8cb269f0495
MariRuizC <123032123+mariruizcruz@users.noreply.github.com>
2024-03-19 22:54:48 -0600

    se crean funciones

--- PortalNoticasDB.sql
+++ PortalNoticasDB.sql
@@ -109,7 +109,7 @@ CREATE TABLE Calificaciones (
     id_usuario NUMBER,
     calificacion NUMBER(1, 0), -- De 1 a 5 estrellas
     FOREIGN KEY (id_noticia) REFERENCES Noticias(id_noticia),
-    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
+  <  FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 );
 
 -- Insert de datos tabla calificaciones
@@ -282,6 +282,7 @@ CREATE TABLE ImagenesNoticias (
     FROM Noticias
     ORDER BY fecha_publicacion DESC;
 
+    SELECT * FROM Vista_Noticias_Recientes;
 
 --Vista de temas y subtemas
 
@@ -290,6 +291,7 @@ CREATE TABLE ImagenesNoticias (
     FROM Temas t
     INNER JOIN Subtemas s ON t.id_tema = s.id_tema;
 
+    SELECT * FROM Vista_Temas_Subtemas;
 
 --Noticias con calificaciones
 
@@ -299,6 +301,7 @@ CREATE TABLE ImagenesNoticias (
     LEFT JOIN Calificaciones c ON n.id_noticia = c.id_noticia
     GROUP BY n.id_noticia, n.titulo;
 
+    SELECT * FROM Vista_Noticias_Calificaciones;
 
 --Visitas por usuarios
 
@@ -307,6 +310,8 @@ CREATE TABLE ImagenesNoticias (
     FROM Visitas
     GROUP BY id_usuario;
 
+    SELECT * FROM Vista_Visitas_Usuario;
+
 --Comentarios recientes en noticias
 
     CREATE VIEW Vista_Noticias_Comentarios AS
@@ -314,3 +319,366 @@ CREATE TABLE ImagenesNoticias (
     FROM Noticias n
     LEFT JOIN Comentarios c ON n.id_noticia = c.id_noticia
     ORDER BY c.fecha_comentario DESC;
+
+    SELECT * FROM Vista_Noticias_Comentarios;
+
+--Vista noticias por ranking
+
+    CREATE VIEW Vista_Ranking_Noticias AS
+    SELECT n.id_noticia, n.titulo, r.visitas, r.promedio_calificaciones
+    FROM Noticias n
+    INNER JOIN Ranking r ON n.id_noticia = r.id_noticia
+    ORDER BY r.visitas DESC, r.promedio_calificaciones DESC;
+
+    SELECT * FROM Vista_Ranking_Noticias;
+
+--Vista de Usuarios con numero de comentarios hechos 
+
+    CREATE VIEW Vista_Usuarios_Comentarios AS
+    SELECT u.id_usuario, u.nombre AS nombre_usuario, COUNT(c.id_comentario) AS cantidad_comentarios
+    FROM Usuarios u
+    LEFT JOIN Comentarios c ON u.id_usuario = c.id_usuario
+    GROUP BY u.id_usuario, u.nombre;
+
+    SELECT * FROM Vista_Usuarios_Comentarios;
+
+--Vista de Noticias mas comentadas
+
+    CREATE VIEW Vista_Noticias_Populares AS
+    SELECT n.id_noticia, n.titulo, COUNT(c.id_comentario) AS cantidad_comentarios
+    FROM Noticias n
+    LEFT JOIN Comentarios c ON n.id_noticia = c.id_noticia
+    GROUP BY n.id_noticia, n.titulo
+    ORDER BY COUNT(c.id_comentario) DESC;    
+
+    SELECT * FROM Vista_Noticias_Populares;
+
+--Vista de noticias con imagenes
+
+    CREATE VIEW Vista_Noticias_Imagenes AS
+    SELECT n.id_noticia, n.titulo, COUNT(in.id_imagen) AS cantidad_imagenes
+    FROM Noticias n
+    LEFT JOIN ImagenesNoticias in ON n.id_noticia = in.id_noticia
+    GROUP BY n.id_noticia, n.titulo;
+
+    SELECT * FROM Vista_Noticias_Imagenes;
+
+--Vista de Promedio de calificaciones y comentarios en noticias (esta no se si sirva .-.)
+
+    CREATE VIEW Vista_Noticias_Calificaciones_Comentarios AS
+    SELECT n.id_noticia, n.titulo, AVG(c.calificacion) AS promedio_calificacion, COUNT(co.id_comentario) AS cantidad_comentarios
+    FROM Noticias n
+    LEFT JOIN Calificaciones c ON n.id_noticia = c.id_noticia
+    LEFT JOIN Comentarios co ON n.id_noticia = co.id_noticia
+    GROUP BY n.id_noticia, n.titulo;
+
+    SELECT * FROM Vista_Noticias_Calificaciones_Comentarios;
+
+
+------------------------------- Funciones  ----------------------------
+
+-- Función para insertar un nuevo usuario
+    CREATE OR REPLACE FUNCTION insertar_usuario(
+        p_id_usuario IN NUMBER,
+        p_nombre IN VARCHAR2,
+        p_correo IN VARCHAR2,
+        p_contrasena IN VARCHAR2
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        INSERT INTO Usuarios (id_usuario, nombre, correo, contrasena)
+        VALUES (p_id_usuario, p_nombre, p_correo, p_contrasena);
+        
+        RETURN 'Usuario insertado correctamente';
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RETURN 'Error: El correo electrónico ya está en uso';
+        WHEN OTHERS THEN
+            RETURN 'Error: Ocurrió un error al insertar el usuario';
+    END;
+    /
+
+-- Función para recuperar el nombre de un usuario dado su ID
+    CREATE OR REPLACE FUNCTION obtener_nombre_usuario(
+        p_id_usuario IN NUMBER
+    ) RETURN VARCHAR2 IS
+        v_nombre Usuarios.nombre%TYPE;
+    BEGIN
+        SELECT nombre INTO v_nombre FROM Usuarios WHERE id_usuario = p_id_usuario;
+        RETURN v_nombre;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN 'Usuario no encontrado';
+        WHEN OTHERS THEN
+            RETURN 'Error al recuperar el nombre del usuario';
+    END;
+    
+-- Función para insertar un nuevo tema
+    CREATE OR REPLACE FUNCTION insertar_tema(
+        p_id_tema IN NUMBER,
+        p_nombre IN VARCHAR2,
+        p_descripcion IN VARCHAR2
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        INSERT INTO Temas (id_tema, nombre, descripcion)
+        VALUES (p_id_tema, p_nombre, p_descripcion);
+        
+        RETURN 'Tema insertado correctamente';
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RETURN 'Error: El ID del tema ya está en uso';
+        WHEN OTHERS THEN
+            RETURN 'Error: Ocurrió un error al insertar el tema';
+    END;
+
+-- Función para obtener la descripción de un tema dado su ID
+    CREATE OR REPLACE FUNCTION obtener_descripcion_tema(
+        p_id_tema IN NUMBER
+    ) RETURN VARCHAR2 IS
+        v_descripcion Temas.descripcion%TYPE;
+    BEGIN
+        SELECT descripcion INTO v_descripcion FROM Temas WHERE id_tema = p_id_tema;
+        RETURN v_descripcion;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN 'Tema no encontrado';
+        WHEN OTHERS THEN
+            RETURN 'Error al recuperar la descripción del tema';
+    END;    
+
+-- Función para insertar un nuevo tema
+    CREATE OR REPLACE FUNCTION insertar_tema(
+        p_id_tema IN NUMBER,
+        p_nombre IN VARCHAR2,
+        p_descripcion IN VARCHAR2
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        INSERT INTO Temas (id_tema, nombre, descripcion)
+        VALUES (p_id_tema, p_nombre, p_descripcion);
+        
+        RETURN 'Tema insertado correctamente';
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RETURN 'Error: El ID de tema ya está en uso';
+        WHEN OTHERS THEN
+            RETURN 'Error: Ocurrió un error al insertar el tema';
+    END;
+
+-- Funcion para insertar nuevo subtema
+    CREATE OR REPLACE FUNCTION insertar_subtema(
+        p_id_subtema IN NUMBER,
+        p_nombre IN VARCHAR2,
+        p_descripcion IN VARCHAR2,
+        p_id_tema IN NUMBER
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        INSERT INTO Subtemas (id_subtema, nombre, descripcion, id_tema)
+        VALUES (p_id_subtema, p_nombre, p_descripcion, p_id_tema);
+        
+        RETURN 'Subtema insertado correctamente';
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RETURN 'Error: El ID de subtema ya está en uso';
+        WHEN OTHERS THEN
+            RETURN 'Error: Ocurrió un error al insertar el subtema';
+    END;
+
+-- Funcion para insertar una nueva noticia
+    CREATE OR REPLACE FUNCTION insertar_noticia(
+        p_id_noticia IN NUMBER,
+        p_titulo IN VARCHAR2,
+        p_contenido IN CLOB,
+        p_fecha_publicacion IN DATE,
+        p_id_tema IN NUMBER,
+        p_id_subtema IN NUMBER,
+        p_id_usuario IN NUMBER
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        INSERT INTO Noticias (id_noticia, titulo, contenido, fecha_publicacion, id_tema, id_subtema, id_usuario)
+        VALUES (p_id_noticia, p_titulo, p_contenido, p_fecha_publicacion, p_id_tema, p_id_subtema, p_id_usuario);
+        
+        RETURN 'Noticia insertada correctamente';
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RETURN 'Error: El ID de noticia ya está en uso';
+        WHEN OTHERS THEN
+            RETURN 'Error: Ocurrió un error al insertar la noticia';
+    END;
+
+-- Funcion para obtener el contenido de una noticia por ID:
+    CREATE OR REPLACE FUNCTION obtener_contenido_noticia(
+        p_id_noticia IN NUMBER
+    ) RETURN CLOB IS
+        v_contenido Noticias.contenido%TYPE;
+    BEGIN
+        SELECT contenido INTO v_contenido FROM Noticias WHERE id_noticia = p_id_noticia;
+        RETURN v_contenido;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN NULL;
+        WHEN OTHERS THEN
+            RETURN 'Error al obtener el contenido de la noticia';
+    END;
+
+-- Función para insertar una nueva calificación
+    CREATE OR REPLACE FUNCTION insertar_calificacion(
+        p_id_calificacion IN NUMBER,
+        p_id_noticia IN NUMBER,
+        p_id_usuario IN NUMBER,
+        p_calificacion IN NUMBER
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        IF p_calificacion < 1 OR p_calificacion > 5 THEN
+            RETURN 'Error: La calificación debe estar entre 1 y 5 estrellas';
+        ELSE
+            INSERT INTO Calificaciones (id_calificacion, id_noticia, id_usuario, calificacion)
+            VALUES (p_id_calificacion, p_id_noticia, p_id_usuario, p_calificacion);
+            
+            RETURN 'Calificación insertada correctamente';
+        END IF;
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RETURN 'Error: El ID de calificación ya está en uso';
+        WHEN OTHERS THEN
+            RETURN 'Error: Ocurrió un error al insertar la calificación';
+    END;
+
+-- Función para obtener la calificación promedio de una noticia:
+    CREATE OR REPLACE FUNCTION obtener_calificacion_promedio(
+        p_id_noticia IN NUMBER
+    ) RETURN NUMBER IS
+        v_promedio NUMBER;
+    BEGIN
+        SELECT AVG(calificacion) INTO v_promedio FROM Calificaciones WHERE id_noticia = p_id_noticia;
+        RETURN v_promedio;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN NULL;
+        WHEN OTHERS THEN
+            RETURN -1; -- Indicador de error
+    END;
+
+-- Función para registrar una nueva visita
+    CREATE OR REPLACE FUNCTION registrar_visita(
+        p_id_visita IN NUMBER,
+        p_id_noticia IN NUMBER,
+        p_id_usuario IN NUMBER
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        INSERT INTO Visitas (id_visita, id_noticia, id_usuario)
+        VALUES (p_id_visita, p_id_noticia, p_id_usuario);
+        
+        RETURN 'Visita registrada correctamente';
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RETURN 'Error: El ID de visita ya está en uso';
+        WHEN OTHERS THEN
+            RETURN 'Error: Ocurrió un error al registrar la visita';
+    END;
+
+-- Funcion que insetar un nuevo envío de correo     
+    CREATE OR REPLACE FUNCTION insertar_envio_correo(
+        p_id_noticia IN NUMBER,
+        p_id_usuario_envia IN NUMBER,
+        p_id_usuario_recibe IN NUMBER
+    ) RETURN NUMBER IS
+    BEGIN
+        INSERT INTO EnviosCorreo (id_envio, id_noticia, id_usuario_envia, id_usuario_recibe, fecha_envio)
+        VALUES (seq_envios_correo.nextval, p_id_noticia, p_id_usuario_envia, p_id_usuario_recibe, SYSDATE);
+
+        RETURN seq_envios_correo.currval; -- Devuelve el ID del envío de correo recién insertado
+    END insertar_envio_correo;
+
+-- Función para obtener el número de visitas de una noticia
+    CREATE OR REPLACE FUNCTION obtener_numero_visitas(
+        p_id_noticia IN NUMBER
+    ) RETURN NUMBER IS
+        v_numero_visitas NUMBER;
+    BEGIN
+        SELECT COUNT(*) INTO v_numero_visitas FROM Visitas WHERE id_noticia = p_id_noticia;
+        RETURN v_numero_visitas;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN 0;
+        WHEN OTHERS THEN
+            RETURN -1; -- Indicador de error
+    END;
+
+-- Funcion para promedio de calificaciones para una noticia
+    CREATE OR REPLACE FUNCTION calcular_promedio_calificaciones(id_noticia_param IN NUMBER)
+    RETURN NUMBER IS
+        promedio NUMBER;
+    BEGIN
+        SELECT AVG(promedio_calificaciones)
+        INTO promedio
+        FROM Ranking
+        WHERE id_noticia = id_noticia_param;
+
+        RETURN promedio;
+    END calcular_promedio_calificaciones;
+
+--Funcion para insertar un nuevo comentario
+    CREATE OR REPLACE FUNCTION insertar_comentario(
+        id_noticia_param IN NUMBER,
+        id_usuario_param IN NUMBER,
+        comentario_param IN VARCHAR2
+    ) RETURN NUMBER IS
+    BEGIN
+        INSERT INTO Comentarios (id_comentario, id_noticia, id_usuario, comentario, fecha_comentario)
+        VALUES (seq_comentarios.nextval, id_noticia_param, id_usuario_param, comentario_param, SYSDATE);
+
+        RETURN seq_comentarios.currval; -- Devuelve el ID del comentario recién insertado
+    END insertar_comentario;
+
+--Funcion para insertar un nuevo registro de seguimiento de usuario
+    CREATE OR REPLACE FUNCTION registrar_accion_seguimiento(
+        id_usuario_param IN NUMBER,
+        id_noticia_param IN NUMBER,
+        tipo_accion_param IN VARCHAR2
+    ) RETURN NUMBER IS
+    BEGIN
+        INSERT INTO SeguimientoUsuarios (id_seguimiento, id_usuario, id_noticia, tipo_accion, fecha_accion)
+        VALUES (seq_seguimiento_usuarios.nextval, id_usuario_param, id_noticia_param, tipo_accion_param, SYSDATE);
+
+        RETURN seq_seguimiento_usuarios.currval; -- Devuelve el ID del seguimiento recién insertado
+    END registrar_accion_seguimiento;
+
+
+------------------------------- Trigers  ----------------------------
+
+--Actualizar ranking de noticias
+    CREATE OR REPLACE TRIGGER actualizar_ranking_visitas
+    AFTER INSERT ON Visitas
+    FOR EACH ROW
+    BEGIN
+        UPDATE Ranking
+        SET visitas = visitas + 1
+        WHERE id_noticia = :new.id_noticia;
+    END;
+
+-- Promedio de las noticias
+    CREATE OR REPLACE TRIGGER calcular_promedio_calificaciones
+    AFTER INSERT ON Calificaciones
+    FOR EACH ROW
+    BEGIN
+        UPDATE Ranking r
+        SET promedio_calificaciones = (SELECT AVG(calificacion) FROM Calificaciones WHERE id_noticia = :new.id_noticia)
+        WHERE r.id_noticia = :new.id_noticia;
+    END;
+
+--Fecha de la ultima visita del usuario
+    CREATE OR REPLACE TRIGGER actualizar_ultima_visita_usuario
+    AFTER INSERT ON Visitas
+    FOR EACH ROW
+    BEGIN
+        UPDATE Usuarios
+        SET fecha_ultima_visita = SYSDATE
+        WHERE id_usuario = :new.id_usuario;
+    END;
+
+--Registra cuando se publico una noticia en la tabla cuando se entra un registro nuevo 
+    CREATE OR REPLACE TRIGGER registrar_fecha_publicacion
+    BEFORE INSERT ON Noticias
+    FOR EACH ROW
+    BEGIN
+        :new.fecha_publicacion := SYSDATE;
+    END;
